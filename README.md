7ef6d6587c24d093f8b1012755db2007356e112b
ewjlambert <ewjlambert@gmail.com>
2021-12-03 16:26:27 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-jzu3yt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-jzu3yt)
---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./style.css";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+  
+
+      export default function BasicGrid() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Item>xs=8</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>xs=8</Item>
+        </Grid>
+      </Grid>
+    </Box>
+ );
+}
+
+    </div>
+
+
+
+
+
+  );
+}
